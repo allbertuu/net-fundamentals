103c57471ffc5838b35836e528867c92b5a70d1e
Alberto <albertovinicius3@gmail.com>
2024-04-09 18:36:42 -0300

    feat: `class Pessoa`

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using net_fundamentals.Models;
+
+Pessoa alberto = new()
+{
+    Nome = "Alberto",
+    Idade = 21
+};
+
+alberto.Apresentar();
