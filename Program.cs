0ac642d3225d4f2a182128de14c0ea2a1339018c
Alberto <albertovinicius3@gmail.com>
2024-04-15 12:17:44 -0300

    feat: operadores aritméticos

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
 ﻿using net_fundamentals.Models;
 
-Pessoa alberto = new()
-{
-    Nome = "Alberto",
-    Idade = 21
-};
+// double raiz = Math.Sqrt(25);
+// Console.WriteLine(raiz);
 
-alberto.Apresentar();
+// double potencia = Math.Pow(5, 2);
+// Console.WriteLine(potencia);
+
+// if (2 > 1)
+// {
+//     Console.WriteLine("Deu bom");
+// } else {
+//     Console.WriteLine("Deu ruim");
+// }
+
+
+// int a = Convert.ToInt32("5");
+// int b = int.Parse("5");
+// string c = 5.ToString();
+
+
+// string meuNome = "nome qualquer";
+// int minhaIdade = 20;
+// DateTime dataNascimento = new(2003, 4, 2);
+
+// Console.WriteLine(dataNascimento);
+
+// Pessoa alberto = new()
+// {
+//     Nome = "Alberto",
+//     Idade = 21
+// };
+
+// alberto.Apresentar();
