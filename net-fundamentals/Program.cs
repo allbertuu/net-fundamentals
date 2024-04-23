231b857cb3f098c218959c0c638322e1cf8d6dc9
Alberto <albertovinicius3@gmail.com>
2024-04-23 17:17:20 -0300

    feat: arrays e listas

--- net-fundamentals/Program.cs
+++ net-fundamentals/Program.cs
@@ -1,12 +1,25 @@
 ﻿using net_fundamentals.Commom.Models;
 
-Pessoa pessoa = new()
-{
-  Nome = "Alberto",
-  Idade = 21
-};
+// List<string> estados = ["BA", "SP"];
+// Console.WriteLine($"Nº de elementos na lista: {estados.Count}");
+// estados.Add("MG");
+// Console.WriteLine($"Nº de elementos na lista APOS add MG: {estados.Count}");
+// estados.Remove("BA");
+// Console.WriteLine($"Nº de elementos na lista APOS remover BA: {estados.Count}");
 
-pessoa.Apresentar();
+// int[] listaDePotenciasDoCincoAoQuadrado = [5, 25, 125, 3125];
+
+// int contador = 0;
+// foreach (int potenciaAoQuadrado in listaDePotenciasDoCincoAoQuadrado)
+// {
+//   Console.WriteLine($"O Nº da potencia é: {potenciaAoQuadrado} - Posição {contador}");
+//   contador++;
+// }
+
+// //redimensionar array - somente cria uma cópia do array e joga pra ele de novo. 
+// //Obs.: dá para usar Array.Copy também
+// Array.Resize(ref listaDePotenciasDoCincoAoQuadrado, listaDePotenciasDoCincoAoQuadrado.Length * 2);
+
 
 // double raiz = Math.Sqrt(25);
 // Console.WriteLine(raiz);
